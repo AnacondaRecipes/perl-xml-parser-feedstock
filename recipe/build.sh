1a849b161e8f14265d5adebfb23555473a9dfbab
Kai Tietz <ktietz@anaconda.com>
2022-10-27 17:23:45 +0200

    create symlink for gcc linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-#chmod 644 samples/{canonical,xml*}
-#perl -pi -e 's|^#!/usr/local/bin/perl\b|#!${PREFIX}/bin/perl|' samples/{canonical,xml*}
-
-# Remove bundled library
-#rm -r inc
-#sed -i -e '/^inc\// d' MANIFEST
-
+ln -s ${CC} gcc
+export PATH=$PWD:$PATH
 
 perl Makefile.PL EXPATLIBPATH=$PREFIX/lib EXPATINCPATH=$PREFIX/include INSTALLDIRS=site
 make
