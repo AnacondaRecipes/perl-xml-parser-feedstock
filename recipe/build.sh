af34ae90f822c6bf8217b10a831c043d05e314f9
Kai Tietz <ktietz@anaconda.com>
2022-10-27 17:09:19 +0200

    revert change

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-chmod 644 samples/{canonical,xml*}
-perl -pi -e 's|^#!/usr/local/bin/perl\b|#!${PREFIX}/bin/perl|' samples/{canonical,xml*}
+#chmod 644 samples/{canonical,xml*}
+#perl -pi -e 's|^#!/usr/local/bin/perl\b|#!${PREFIX}/bin/perl|' samples/{canonical,xml*}
 
 # Remove bundled library
-rm -r inc
-sed -i -e '/^inc\// d' MANIFEST
+#rm -r inc
+#sed -i -e '/^inc\// d' MANIFEST
 
 
 perl Makefile.PL EXPATLIBPATH=$PREFIX/lib EXPATINCPATH=$PREFIX/include INSTALLDIRS=site
