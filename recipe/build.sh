899c6d7e7f8c23dc04447207a4a60bb08e588473
Kai Tietz <ktietz@anaconda.com>
2022-10-27 16:40:43 +0200

    adjustments for building

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+chmod 644 samples/{canonical,xml*}
+perl -pi -e 's|^#!/usr/local/bin/perl\b|#!${PREFIX}/bin/perl|' samples/{canonical,xml*}
+
+# Remove bundled library
+rm -r inc
+sed -i -e '/^inc\// d' MANIFEST
+
+
 perl Makefile.PL EXPATLIBPATH=$PREFIX/lib EXPATINCPATH=$PREFIX/include INSTALLDIRS=site
 make
 make install
