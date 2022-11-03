c8cd266367d735251b5e9927118a53104b587ff4
Kai Tietz <ktietz@anaconda.com>
2022-11-03 10:39:10 +0100

    remove set -ex

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-set -ex
-
 if [[ $(uname) == "Linux" ]]; then
     echo "adding gcc/gxx symlinks ${GCC} / ${GXX}"
-    #ln -s ${GXX} ./g++
-    #ln -s ${GCC} ./gcc
+    ln -s ${GXX} ./g++
+    ln -s ${GCC} ./gcc
 
-    #export PATH=$PWD:$PATH
+    export PATH=$PWD:$PATH
 fi
 
+echo "Invoking ${PREFIX}/bin/perl ..."
 $PREFIX/bin/perl Makefile.PL EXPATLIBPATH=${PREFIX}/lib EXPATINCPATH=${PREFIX}/include INSTALLDIRS=site
 make
 make install
