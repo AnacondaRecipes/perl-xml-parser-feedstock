67184f748a00ef484e251c62b370ef54b7a37fc7
Kai Tietz <ktietz@anaconda.com>
2022-11-03 10:42:04 +0100

    ...

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-if [[ $(uname) == "Linux" ]]; then
-    echo "adding gcc/gxx symlinks ${GCC} / ${GXX}"
-    ln -s ${GXX} ./g++
-    ln -s ${GCC} ./gcc
+echo "adding cc/cxx symlinks ${CC} / ${CXX}"
+    ln -s ${CXX} ./g++
+    ln -s ${CC} ./gcc
 
     export PATH=$PWD:$PATH
-fi
 
 echo "Invoking ${PREFIX}/bin/perl ..."
 $PREFIX/bin/perl Makefile.PL EXPATLIBPATH=${PREFIX}/lib EXPATINCPATH=${PREFIX}/include INSTALLDIRS=site
