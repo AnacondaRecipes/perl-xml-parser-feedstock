9bd7fc4ac8ac502a4c00ec9584ad70a0fcf50098
Kai Tietz <ktietz@anaconda.com>
2022-11-03 10:45:26 +0100

    debug

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 echo "adding cc/cxx symlinks ${CC} / ${CXX}"
-    ln -s ${CXX} ./g++
-    ln -s ${CC} ./gcc
+ln -s ${CXX} ./g++
+ln -s ${CC} ./gcc
 
-    export PATH=$PWD:$PATH
+export PATH=$PWD:$PATH
 
 echo "Invoking ${PREFIX}/bin/perl ..."
-$PREFIX/bin/perl Makefile.PL EXPATLIBPATH=${PREFIX}/lib EXPATINCPATH=${PREFIX}/include INSTALLDIRS=site
+ls -la ${PREFIX}/bin
+
+${PREFIX}/bin/perl Makefile.PL EXPATLIBPATH=${PREFIX}/lib EXPATINCPATH=${PREFIX}/include INSTALLDIRS=site
 make
 make install
