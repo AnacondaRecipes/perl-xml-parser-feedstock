a09bef34e6f93d0262947cb59801a9055cb99cd6
Kai Tietz <ktietz@anaconda.com>
2022-11-03 10:16:44 +0100

    attempt to fix linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+i
+if [[ $(uname) == "Linux" ]]; then
+    ln -s ${GXX} g++ || true
+    ln -s ${GCC} gcc || true
+    ln -s ${USED_BUILD_PREFIX}/bin/${HOST}-gcc-ar gcc-ar || true
 
-ln -s ${CC} ./gcc
-export PATH=$PWD:$PATH
-CC=gcc
+    export LD=${GXX}
+    export CC=${GCC}
+    export CXX=${GXX}
+
+    export PATH=$PWD:$PATH
+fi
 
 $PREFIX/bin/perl Makefile.PL EXPATLIBPATH=${PREFIX}/lib EXPATINCPATH=${PREFIX}/include INSTALLDIRS=site
 make
