bd8070c7d54c4533a4f1186f58b2a490cbcc10ce
Kai Tietz <ktietz@anaconda.com>
2022-11-03 10:26:07 +0100

    adjust

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 i
 if [[ $(uname) == "Linux" ]]; then
-    ln -s ${CXX} g++ || true
+    # ln -s ${CXX} g++ || true
     ln -s ${CC} gcc || true
-
-    export LD=${GXX}
-    export CC=${GCC}
-    export CXX=${GXX}
 
     export PATH=$PWD:$PATH
 fi
