70af1c426f8df3f03167a20eebb11664c5a37d4a
Kai Tietz <ktietz@anaconda.com>
2022-11-08 17:15:00 +0100

    address building on linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,14 +4,9 @@ set -x
 
 cd "$SRC_DIR"
 
-echo "adding cc/cxx symlinks ${CC} / ${CXX}"
-ln -s ${CXX} ./g++
-ln -s ${CC} ./gcc
-
-export PATH=$PWD:$PREFIX/bin:$PATH
+export PATH=$PREFIX/bin:$PATH
 
 echo "Invoking ${PREFIX}/bin/perl ..."
-ls -la ${PREFIX}/bin
 
 ${PREFIX}/bin/perl Makefile.PL EXPATLIBPATH=${PREFIX}/lib EXPATINCPATH=${PREFIX}/include INSTALLDIRS=site
 make
