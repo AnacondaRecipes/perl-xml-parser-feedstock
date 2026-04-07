94dfc4747a35fa94feb56548e19bc0e260bc79da
Eric Lundby <Eric.Lundby@gmail.com>
2026-04-07 16:33:02 -0600

    Try fixing linux-*

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,8 +6,12 @@ cd "$SRC_DIR"
 
 export PATH=$PREFIX/bin:$PATH
 
+export EXPATLIBPATH="${PREFIX}/lib"
+export EXPATINCPATH="${PREFIX}/include"
+export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH:-}"
+
 echo "Invoking ${PREFIX}/bin/perl ..."
 
-${PREFIX}/bin/perl Makefile.PL EXPATLIBPATH=${PREFIX}/lib EXPATINCPATH=${PREFIX}/include INSTALLDIRS=site
+${PREFIX}/bin/perl Makefile.PL INSTALLDIRS=site
 make
 make install
