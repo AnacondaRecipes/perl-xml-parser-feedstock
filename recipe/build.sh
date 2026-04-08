67c0b563c6e9e3417fef589560f19bedd6a48091
Eric Lundby <Eric.Lundby@gmail.com>
2026-04-08 11:30:02 -0600

    Remove hack with updated perl

--- recipe/build.sh
+++ recipe/build.sh
@@ -16,23 +16,5 @@ export PERL_MM_OPT="CC=${CC} LD=${CC}"
 
 ${PREFIX}/bin/perl Makefile.PL INSTALLDIRS=site
 
-# Perl's Config bakes in another build's --sysroot; strip it and end with this env's sysroot.
-if [[ -n "${CONDA_BUILD_SYSROOT:-}" && "${OSTYPE:-}" == linux* ]]; then
-  for mk in "${SRC_DIR}/Makefile" "${SRC_DIR}/Expat/Makefile"; do
-    [[ -f "$mk" ]] || continue
-    sed -i.bak \
-      -e 's|--sysroot=/home/task[^[:space:]]*||g' \
-      -e 's|--sysroot=[^[:space:]]*croot/perl_[^[:space:]]*||g' \
-      -e 's|--sysroot=\$BUILD_PREFIX/[^[:space:]]*||g' \
-      "$mk"
-  done
-  if [[ -f "${SRC_DIR}/Expat/Makefile" ]]; then
-    cat >> "${SRC_DIR}/Expat/Makefile" <<EOF
-
-OPTIMIZE := \$(OPTIMIZE) -isystem ${CONDA_BUILD_SYSROOT}/usr/include --sysroot=${CONDA_BUILD_SYSROOT}
-EOF
-  fi
-fi
-
 make
 make install
